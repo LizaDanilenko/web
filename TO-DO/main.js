7b0dfb5d1d1babc967d0f38c1ce8b070eedc19c4
Elizaveta <Lizadnilenko12@gmail.com>
2025-07-09 20:57:25 +0700

    to-do

---
+++ TO-DO/main.js
@@ -0,0 +1,44 @@
+const todoInput = document.querySelector('.todo__input');
+const todoBtn = document.querySelector('.todo__btn');
+const todoList = document.querySelector('.todo__list');
+
+todoBtn.addEventListener('click', function () {
+    addTask()
+})
+
+function addTask() {
+
+    if(todoInput.value === '') {
+        alert('Нужно ввести текст')
+    } else {
+        let li = document.createElement('li')
+        li.innerHTML = todoInput.value
+        todoList.appendChild(li)
+        let span = document.createElement('span')
+        span.innerHTML = '\u00d7'
+        li.appendChild(span)
+    }
+    todoInput.value = '';
+    saveData()
+}
+
+todoList.addEventListener('click', function (e) {
+
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked')
+        saveData()
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove()
+        saveData()
+    }
+
+})
+
+function saveData() {
+    localStorage.setItem('data', todoList.innerHTML)
+}
+function showData() {
+    todoList.innerHTML = localStorage.getItem('data')
+}
+
+showData();
